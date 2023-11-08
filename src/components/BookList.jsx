7838cf79f5663e5320adc841e04012db97e31687
Michelle Primiani <primianimichelle@gmail.com>
2023-11-08 13:57:36 -0600

    Rendered BookList and light css styling

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,10 +1,44 @@
-
+import { useEffect, useState } from "react";
+import { getAllBooks } from "../services/Books";
 
 export const BookList = () => {
- 
-    return (
-        <main>
-        <h1 className='text-4xl text-center'>Welcome to Readers Digest</h1>
-        </main>
-    )
-}
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getAllBooks().then((bookData) => {
+      setBooks(bookData);
+    });
+  }, []);
+
+  return (
+    <main>
+      <h1 className="text-4xl text-center">Welcome to Readers Digest</h1>
+      <div className="book-list-container flex flex-wrap gap-5 items-center justify-between my-10">
+        {books.map((book) => {
+          return (
+            <div
+              key={book.id}
+              className="books flex items-center basis-80 flex-wrap justify-center text-center"
+              style={{
+                border: "1px solid black", // Soft border
+                borderRadius: "10px", // Small border radius
+                backgroundColor: "#d9f7f7", // Pale blue background color
+                padding: "10px", // Add some padding for spacing
+              }}
+            >
+              <div className="book-title text-2xl px-12 py-2">
+                {book.title}
+              </div>
+              <div className="book-author font-light">Author: {book.author}</div>
+              <img
+                className="book_img"
+                src={book.cover_image}
+                alt={book.title}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+};
